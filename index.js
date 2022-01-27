f81b8e03c990c9d0f92ba1403659669d41431024
Nesat-Nayem <nesatnayem042@gmail.com>
2022-01-27 23:08:54 +0600

    code add

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 const uri =
-  "mongodb+srv://suntour2:2jS4gDHXFjCCYG7p@cluster0.hty68.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hty68.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
